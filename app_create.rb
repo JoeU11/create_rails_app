d234c9d32c1bd96477f8b6e31fb016fb779195e3
joe <joe.ulica@gmail.com>
2022-08-03 11:49:01 -0400

    Add option for db model generation

--- app_create.rb
+++ app_create.rb
@@ -1,12 +1,20 @@
 #This file is an attempt to create some sort of automation for creating a new app
 
+#Upcoming goals to include: 
+# 1) modify seeds.rb file to include format to add instances based on model input. 
+# 2) input at beginning to pick directory to create app.
+# 3) option to select additional gems to bundle with app.
+# 4) make RESTful
+# 5) add support for decimal class in database model
+
+
 #use - place this in the file where you want to create your app and run
 #note - add way to choose directory
 
 p "Enter the name of your app"
 app_name = gets.chomp
 
-
+model = false
 controller = false
 sample_route = false
 
@@ -28,18 +36,53 @@ while true
         break
       end
     end
-
     break
   elsif response == 'n'
     break
   end
 end
 
+while true
+  p "Would you like to generate a database model?(y/n)"
+  response = gets.chomp.downcase
+  if response == 'y'
+    model_vars = []
+    p "What would you like to name your model?"
+    model_name = gets.chomp #make initial cap (or "camal case")
+    p "How many variables would you like to include?"
+    var_num = gets.chomp.to_i
+    count = 1
+    var_num.times do
+      p "What is the name of variable ##{count} (singular please. do not spaces nor ':')" #can add replacement of spaces with "_" and chomp off : and 's' if included
+      var1_name = gets.chomp.downcase
+      p "What type of class is variable ##{count}? (e.g. integer, string)" #can add conditional to ensure type is correct - add array of all possible classes and run .includes?
+      var1_type = gets.chomp.downcase
+      variable = [var1_name, var1_type]
+      model_vars << variable
+      count += 1
+    end
+    p "is the following correct? (y/n)"
+    p model_vars
+    correct = gets.chomp.downcase
+    if correct == "y"
+      model = true
+      break
+    elsif correct == "n"
+    end
+  elsif response == 'n'
+    break
+  end
+end  
+model_command = "rails generate model #{model_name}"
+model_vars.each do |v|
+  model_command = "#{model_command} #{v[0]}:#{v[1]} "
+  p v
+end
+
 
 system "rails new #{app_name}"
 Dir.chdir "#{app_name}/"
 system "rails db:create"
-system "rails db:migrate"
 if controller
   system "rails generate controller #{controller_name}"
 end
@@ -57,6 +100,10 @@ if sample_route
     file.rewind
     file.write(lines.join)
   end
+end
+if model
+  system model_command
+  system "rails db:migrate"
 end
 
 
